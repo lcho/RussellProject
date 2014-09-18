3fb8c2968cbfe42142857fa13a0e2961dfcd954d
lcho <cholance1022@gmail.com>
2014-09-18 10:59:23 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
-/******************************************************************************
- *  @file main.cpp
- *  @description Main or Driver file for the custom linked list
- *  @author Lance Cho
- *****************************************************************************
- */
+/****************************************************************************
+ *  @file main.cpp                                                          *
+ *  @description Main or Driver file for the custom linked list             *
+ *  @author Lance Cho                                                       *
+ ****************************************************************************/
 
 #include "stock.h"
 #include "list.h"
@@ -14,29 +13,18 @@ using namespace std;
 int main(int argc, const char * argv[])
 {
     List<Stock> stock;
-    string data;
     ifstream infile("StockList.csv");
+    ofstream outfile("StockListEdited.csv");
+    
     if (!infile) {
         cout << "File could not be opened." << endl;
         return 1;
     } else {
-        // build linked list using *filename* and *linkedlist_name*
-        stock.buildList(infile, stock);
+        // Helper method that performs all necessary implementations
+        //  in the background
+        stock.mainOperationsHelper(infile, outfile);
     }
-    
-    // print out the linked list
-//    cout << "Stocks: " << stock << endl;
-    
-    stock.printCommonavg();
-    stock.printADRavg();
-    stock.removeADR();
-//    cout << "\n\nStocks After Remove: " << stock << endl;
-    
-    cout << "Enter a ticker ([!quit] to quit): " << endl;
-    cin >> stock;
-    
-    cout << "Printed the updated list on a csv file" << endl;
-    cout << "Have a nice day!" << endl;
+
     return 0;
 }
 
